69df4e5e1b7007e1da659e3e90e765d170696170
ly10yue <1990596485@qq.com>
2025-11-03 17:49:36 +0800

    Refactor power and combination functions with MOD

--- completed/29.c
+++ completed/29.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#define MOD 1000000007
 long long powh(long long a, long long b)
 {
     long long res = 1;
     while (b){
         if (b & 1)
-            res = res * a % 1000000007;
-        a = a * a % 1000000007;
+            res = res * a % MOD;
+        a = a * a % MOD;
         b >>= 1;
     }
     return res;
@@ -15,14 +16,14 @@ long long count(long long t[], int i, int j)
     long long cou = 0;
     for (int x = i, y = j; x <= y; x++)
         cou = cou + t[x];
-    return cou % 1000000007;
+    return cou % MOD;
 }
 long long C[1001][1001];
 void init_comb(){
     for (int i = 0; i <= 1000; i++){
         C[i][0] = C[i][i] = 1;
         for (int j = 1; j < i; j++)
-            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % 1000000006;
+            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % (MOD-1);
     }
 }
 
@@ -54,8 +55,8 @@ int main()
         {
             if (trans(n, i, j, m - 1))
             {
-                long long cc = trans(n, i, j, m - 1) % 1000000006;
-                ans = (ans * powh(count(t, i, j), cc)) % 1000000007;
+                long long cc = trans(n, i, j, m - 1) % (MOD-1);
+                ans = (ans * powh(count(t, i, j), cc)) % MOD;
             }
         }
     }
@@ -65,71 +66,3 @@ int main()
         printf("%lld", ans);
 }
 
-/*#include <stdio.h>
-long long powh(long long a, long long b)
-{
-    long long res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % 1000000007;
-        a = a * a % 1000000007;
-        b >>= 1;
-    }
-    return res;
-}
-long long count(long long t[], int i, int j)
-{
-    long long cou = 0;
-    for (int x = i, y = j; x <= y; x++)
-        cou = cou + t[x];
-    return cou % 1000000007;
-}
-long long C[1001][1001];
-void init_comb()
-{
-    for (int i = 0; i <= 1000; i++)
-    {
-        C[i][0] = C[i][i] = 1;
-        for (int j = 1; j < i; j++)
-            C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]) % 1000000006;
-    }
-}
-long long trans(int n, int i, int j, int cut)
-{
-    int remain = n - j + i - 1;
-    int piece = 2;
-    if (i == 0)
-        piece--;
-    if (j == n - 1)
-        piece--;
-    if (piece == 0 || cut < 0)
-        return 0;
-    else
-        return C[remain - piece][cut - piece];
-}
-int main()
-{
-    init_comb();
-    int n, m;
-    scanf("%d%d", &n, &m);
-    long long t[n];
-    long long ans = 1;
-    for (int i = 0; i < n; i++)
-        scanf("%lld", &t[i]);
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
-        {
-            if (trans(n, i, j, m - 1))
-            {
-                long long cc = trans(n, i, j, m - 1) % 1000000006;
-                ans = (ans * powh(count(t, i, j), cc)) % 1000000007;
-            }
-        }
-    }
-    if (m == 1)
-        printf("%lld", count(t, 0, n - 1));
-    else
-        printf("%lld", ans);
-}*/
